d63066c5bf6d92540beb7315358c26da84bf7681
yummicodes <yummicodes@gmail.com>
2021-06-12 09:02:37 +0700

    Add File Dicoding_MyIntentApp

---
+++ app/src/main/java/com/example/dicoding_myintentapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.dicoding_myintentapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnMoveActivity: Button
+    private lateinit var btnMoveWithData: Button
+    private lateinit var btnDialANumber: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnMoveActivity = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+    }
+
+
+}
