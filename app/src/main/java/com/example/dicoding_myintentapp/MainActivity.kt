a8441cea8d05babae197eed5e7867691d56e55f1
yummicodes <yummicodes@gmail.com>
2021-06-12 10:57:30 +0700

    Add dial a number & formatting line

--- app/src/main/java/com/example/dicoding_myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/dicoding_myintentapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dicoding_myintentapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -21,20 +22,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         btnMoveWithData = findViewById(R.id.btn_move_with_data)
         btnMoveWithData.setOnClickListener(this)
+
+        btnDialANumber = findViewById(R.id.btn_dial)
+        btnDialANumber.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        when (v?.id){
+        when (v?.id) {
             R.id.btn_move_activity -> {
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
             }
 
             R.id.btn_move_with_data -> {
-                val moveWithDataIntent = Intent(this@MainActivity,MoveWithDataActivity::class.java)
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Yummicodes")
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 19)
                 startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_dial -> {
+                val phoneNumber = "081775205889"
+                val dialPhoneNumber = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber))
+                startActivity(dialPhoneNumber)
             }
 
         }
